05b9f6f1c90345fb8f3c2bb9242d0c1304dc3efa
Владимир Ливинский <xDamneDx@Gmail.com>
2020-11-02 16:38:34 +0200

    Completed the chapter 'Getting Data into Gatsby with GraphQL'

--- gatsby/.cache/api-runner-ssr.js
+++ gatsby/.cache/api-runner-ssr.js
@@ -1,4 +1,10 @@
 var plugins = [{
+      plugin: require('/Users/xdamnedx/Documents/WebDev/Master-Gatsby/gatsby/node_modules/gatsby-plugin-styled-components/gatsby-ssr'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/xdamnedx/Documents/WebDev/Master-Gatsby/gatsby/node_modules/gatsby-source-sanity/gatsby-ssr'),
+      options: {"plugins":[],"projectId":"vttml911","dataset":"production","watchMode":true,"token":"skS3gFq6SONTldo6bRqEPVb1pdo67ph9eu5N2EB952cqeKzQpxy0G0Et1CeK8vKNjMD4pCHzVHvpZtX6U5l6eeX8owX7rnhkX6ZLwQots4q6cSmT0taCQLHObnDcgtH81HP7aMh7S88ARGryOCsctKkKmFwLmarJRXAl1fz8DdIgsgGq4I51"},
+    },{
       plugin: require('/Users/xdamnedx/Documents/WebDev/Master-Gatsby/gatsby/gatsby-ssr'),
       options: {"plugins":[]},
     }]
